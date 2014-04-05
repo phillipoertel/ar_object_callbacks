b9bd1665d0e745b5f8561b53bd8d6bf8f9afd31d
Phillip Oertel <me@phillipoertel.com>
2014-04-05 10:20:18 +0200

    First commit. rspec present

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArObjectCallbacks::Application.config.secret_key_base = 'df230fc2fd2bafcf1fe23d8dac1098a27c43b5bbba16b781f8384cd8ece32a5e0767b02866b26eada4af5a34e3174c49d6fc31c2d6b6825caf4eba1ae67a8a64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArObjectCallbacks::Application.config.session_store :cookie_store, key: '_ar_object_callbacks_session'
